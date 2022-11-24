ee9e143105f063724870dfaad18570a460ba057d
SpaghettiCodeCa <tommyaudet@spaghetticode.ca>
2022-11-24 15:29:32 -0500

    ajout de syst'eme d'authentification + authorisation token avec jwt

--- server/index.js
+++ server/index.js
@@ -8,6 +8,8 @@ import helmet from 'helmet';
 import morgan from 'morgan';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import authRoutes from '.routes/auth.js';
+import { register } from "./controllers/auth.js";
 
 /* CONFIGURATIONS */
 const __filename = fileURLToPath(import.meta.url);
@@ -33,7 +35,13 @@ const storage = multer.diskStorage({
   }
 });
 const upload = multer({storage});
-// console.log(process.env.MONGO_URL);
+
+/* ROUTES WITH FILES */
+app.post("/auth/register", upload.single("picture"), register);
+
+/* ROUTES */
+app.use('/auth', authRoutes);
+
 /* mongoose Setup */
 const PORT = process.env.PORT || 6001;
 mongoose.connect(process.env.MONGO_URL, {
